d435e8552bf171e9fa06ee46ee91dbe78f62884c
Jubraj001 <jubrajdev001@gmail.com>
2023-02-20 18:22:28 +0530

    First Screen Switch done

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native"
 import PrimaryButton from "../components/PrimaryButton"
 
-export default function StartGameScreen() {
+export default function StartGameScreen({onPickNumber}) {
   const [enteredNumber,setEnteredNumber]=useState('');
 
   function numberInputHandler(enteredText){
@@ -22,7 +22,7 @@ export default function StartGameScreen() {
         [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]);
         return;
     }
-    console
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.inputContainer}>
