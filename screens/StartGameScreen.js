60d7ae3b050d2ad3a7b1495cb031493f70bef9f1
Jubraj001 <jubrajdev001@gmail.com>
2023-02-20 10:25:05 +0530

    Linear Gradient

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -10,8 +10,14 @@ export default function StartGameScreen() {
             keyboardType="number-pad" 
             autoCapitalize="none" 
             autoCorrect={false}/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
     </View>
   )
 }
@@ -19,6 +25,7 @@ export default function StartGameScreen() {
 const styles = StyleSheet.create({
     inputContainer:{
         padding:16,
+        alignItems:'center',
         marginTop:100,
         backgroundColor:'#4e0329',
         marginHorizontal:24,
@@ -39,5 +46,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonsContainer:{
+        flexDirection:'row'
+    },
+    buttonContainer:{
+        flex:1
     }
 });
